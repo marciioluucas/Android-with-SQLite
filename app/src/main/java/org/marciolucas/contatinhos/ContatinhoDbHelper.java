6229ecd406521e4175f6e0fc203f4a54ca9fae0a
Marcio Lucas <marciioluucas@gmail.com>
2017-11-09 20:45:28 -0200

    Editando ponto e vírgula que tinha esquecido.

--- app/src/main/java/org/marciolucas/contatinhos/ContatinhoDbHelper.java
+++ app/src/main/java/org/marciolucas/contatinhos/ContatinhoDbHelper.java
@@ -14,24 +14,25 @@ final class ContatinhoDbHelper extends SQLiteOpenHelper {
             ContatinhoContract.COLUNA_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             ContatinhoContract.COLUNA_NOME + " TEXT, " +
             ContatinhoContract.COLUNA_TELEFONE + " TEXT, " +
-            ContatinhoContract.COLUNA_INFO + " TEXT);";
+            ContatinhoContract.COLUNA_INFO + " TEXT); ";
 
     private static final String SQL_DROP = "DROP TABLE IF EXISTS " + ContatinhoContract.NOME_TABELA + ";";
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "agendaCrush.db";
 
     private ContatinhoDbHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION, );
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int vAntiga, int vNova) {
+        db.execSQL(SQL_DROP);
+        onCreate(db);
     }
 }
